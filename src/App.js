153eb4bb9aaad6df6c32b621c43154181c5ce2c7
Joao Vitor <182741@upf.br>
2022-04-19 19:22:08 -0300

    test

---
+++ src/App.js
@@ -0,0 +1,141 @@
+import "./App.css";
+import React, { useState, useEffect, useRef } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import api from "./services/api";
+import ColaboradorList from "./ColaboradorList";
+import ColaboradorForm from "./ColaboradorForm";
+import ColaboradorSrv from "./services/ColaboradorSrv";
+import PrimeReact from 'primereact/api';
+import { Toast } from 'primereact/toast';
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
+
+
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+
+function App() {
+  let colaboradoresList = [
+    { id: 1, nome: "Fulano", email: "email1@teste", senha: "12okp1" },
+    { id: 2, nome: "Beltrano", email: "email2@teste", senha: "1293912" },
+  ];
+
+  const [colaboradores, setColaboradores] = useState(colaboradoresList);
+  const toastRef = useRef();
+  const [usuarios, setUsuarios] = useState([])
+  useEffect(() => {
+    onClickAtualizar(); // ao inicializar execula método para atualizar
+  }, []);
+
+  const onClickAtualizar = () => {
+    ColaboradorSrv.listar().then(response => {
+      setUsuarios(response.data);
+      toastRef.current.show({
+        severity: 'success',
+        summary: "Colaborador atualizados",
+        life: 3000
+      });
+    })
+      .catch(e => {
+        toastRef.current.show({
+          severity: 'error',
+          summary: e.message,
+          life: 3000
+        });
+      });
+  }
+  React.useEffect(() => {
+    api
+      .get("/colaborador")
+      .then((response) => setColaboradores(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+  // operação inserir
+  const initialState = { id: null, nome: '', email: '', senha: '' }
+  const [colaborador, setColaborador] = useState(initialState)
+  const [editando, setEditando] = useState(false)
+
+  const [visible, setVisible] = useState(false);
+  const inserir = () => {
+    setColaborador(initialState);
+    setEditando(true);
+  }
+  const cancelar = () => {
+    console.log('Cancelou ...');
+    setEditando(false);
+  }
+  const salvar = () => {
+    if (colaborador._id == null) { // inclussão
+      ColaboradorSrv.incluir(colaborador).then(response => {
+        setEditando(false);
+        onClickAtualizar();
+        toastRef.current.show({ severity: 'success', summary: "Salvou", life: 2000 });
+      })
+        .catch(e => {
+          toastRef.current.show({ severity: 'error', summary: e.message, life: 4000 });
+        });
+    } else { // alteração
+      ColaboradorSrv.alterar(colaborador).then(response => {
+        setEditando(false);
+        onClickAtualizar();
+        toastRef.current.show({ severity: 'success', summary: "Salvou", life: 2000 });
+      })
+        .catch(e => {
+          toastRef.current.show({ severity: 'error', summary: e.message, life: 4000 });
+        });
+    }
+  }
+
+  const editar = (id) => {
+
+
+  }
+
+  const excluir = (_id) => {
+    confirmDialog({
+      message: 'Confirma a exclusão?',
+      header: 'Confirmação',
+      icon: 'pi pi-question',
+      acceptLabel:'Sim',
+      rejectLabel:'Não',
+      acceptClassName: 'p-button-danger',
+      accept: () => excluirConfirm(_id)
+      });
+    }
+
+  const excluirConfirm = (_id) => {
+    
+    ColaboradorSrv.excluir(_id).then(response => {
+    onClickAtualizar();
+    toastRef.current.show({ severity: 'success',
+    summary: "Excluído", life: 2000 });
+    })
+    .catch(e => {
+    toastRef.current.show({ severity: 'error',
+    summary: e.message, life: 4000 });
+    });
+    }
+
+  if (!editando) {
+    return (
+      <div className="App">
+        <Toast ref={toastRef} />
+        <ConfirmDialog></ConfirmDialog>
+        <ColaboradorList colaboradores={colaboradores}
+          inserir={inserir} editar={editar} excluir={excluir} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <ColaboradorForm colaborador={colaborador} setColaborador={setColaborador}
+          salvar={salvar} cancelar={cancelar} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ColaboradorList.js
@@ -0,0 +1,35 @@
+const ColaboradorList = (props) => (
+    <div>
+      <h4>Listagem de colaboradores</h4>
+      <button className="btn btn-success" onClick={props.inserir}>Inserir</button>
+  
+      <table className="table">
+        <thead>
+          <tr>
+            {" "}
+            <th>Index</th>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Senha</th>
+            <th>Operações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.colaboradores.length > 0 ? (props.colaboradores.map((o, index) => (
+              <tr key={index}>
+                <td>{index}</td><td>{o.id}</td> <td>{o.nome}</td> <td>{o.email}</td><td>{o.senha}</td>
+                <button onClick={() => props.editar(o._id)} className="btn btn-warning">Editar</button>
+                <button onClick={() => props.excluir(o._id)} className="btn btn-danger">Excluir</button> 
+              </tr>
+            ))) : (
+            <tr>
+              <td colSpan={3}>Nenhum colaborador.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+  export default ColaboradorList
+  
